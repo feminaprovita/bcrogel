581f91089f7eef3770aa25e3ce34eeb327feda30
Claire Gilligan <feminaprovita@gmail.com>
2019-11-15 14:54:39 -0500

    fixed registry thanks

---
+++ client/components/RegistryThanks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Registry = () => (
+  <div id='registry-thanks-container' className='page-container'>
+    <h1>Thank you for donating to our registry fund!</h1>
+    <p>Please enjoy the rest of our wedding website! Claire spent a lot of time putting it together, adding features she thought you might enjoy. 😃</p>
+    <img
+    className='small-image'
+    src='./images/Cheung_wedding.jpg'
+    alt="at Clement and Anna's wedding"
+    />
+  </div>
+)
+
+export default Registry
