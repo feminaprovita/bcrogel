a28126f79754afcd24243536985260bf9801bb12
Claire Gilligan <feminaprovita@gmail.com>
2019-11-20 23:59:35 -0500

    added attractions copy, registry links

--- client/components/Registry.js
+++ client/components/Registry.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Registry = () => (
   <div id='registry-container' className='page-container'>
     <h1>Our Registries</h1>
-    <h2>Primary: Zola</h2>
-    <h2>Mostly fun/sports: Bed, Bath, & Beyond</h2>
-    <h2>More: Ikea</h2>
-    <h2>More: Amazon</h2>
-    <h2>Dishwasher $</h2>
-    <h2>Couch $</h2>
-    <h2>Prie-Dieu $</h2>
-    <h2>Honeymoon $</h2>
+    <h3><a href='www.zola.com/registry/swelltoberogel'>Zola</a></h3>
+    <h3><a href='https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/548270669?eventType=Wedding'>Bed, Bath, & Beyond</a> (lots of sports-branded kitchenware here)</h3>
+    <h3><a href='https://info.ikea-usa.com/giftregistry/en-us/registry/5c757d9d-e026-45c4-a760-f3d8167b66c7'>Ikea</a></h3>
+    <h3><a href='https://www.amazon.com/wedding/share/swelltoberogel'>Amazon</a></h3>
+    <h3>Dishwasher $</h3>
+    <h3>Couch $</h3>
+    <h3>Prie-Dieu $</h3>
+    <h3>Honeymoon $</h3>
     <img
       className='under-construction small-image'
       src='./images/under_construction_geek_woman.png'
