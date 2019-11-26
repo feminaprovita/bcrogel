bfdecdef2e17859d7005b6c6d44c79715e1a27c8
Claire Gilligan <feminaprovita@gmail.com>
2019-11-26 16:42:13 -0500

    added db skeleton draft

--- server/db/models/guest.js
+++ server/db/models/guest.js
@@ -2,7 +2,25 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Guest = db.define('guest', {
-  // some stuff here
+  Name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  Related: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+  },
+  RSVP: {
+    type: Sequelize.BOOLEAN,
+    validate: {
+      notEmpty: true
+    }
+  },
+  Special: {
+    type: Sequelize.ENUM('kids', 'priest')
+  }
 })
 
 module.exports = Guest
