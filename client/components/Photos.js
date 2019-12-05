426de8a18c6b7aff680b10556d8d572363573dde
Claire Gilligan <feminaprovita@gmail.com>
2019-12-05 02:03:52 -0500

    more photos, failed to fix sideways steeler game ones

--- client/components/Photos.js
+++ client/components/Photos.js
@@ -52,85 +52,85 @@ const Photos = () => (
           />
           <p className='caption'>Claire's first Penguins game</p>
       </div>
-      <div id='steeler-photo-sad' className='polaroid-sideways'>
+      <div id='steeler-photo-sad' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/steeler_game_first_sad.jpg'
+          src='./images/steeler_game_sad.jpg'
           alt='Silly Steeler gameday photo'
           />
           <p className='caption'>As you've clearly noticed by now, we've been trying to take a picture at every notable sports game. These guys behind us were making faces as we framed our sad photo (because the Steelers were playing poorly)...</p>
       </div>
-      <div id='steeler-photo-happy' className='polaroid-sideways'>
+      <div id='steeler-photo-happy' className='polaroid'>
         <img
-          className='polaroid-image rotate-back'
-          src='./images/steeler_game_first_happy.jpg'
+          className='polaroid-image'
+          src='./images/steeler_game_happy.jpg'
           alt='Silly Steeler gameday photo'
           />
-          <p className='caption rotate-back'>...but we weren't sure they were trolling us until one of us said "Ok, and now a smiling one" and they jumped forward to photobomb us again!</p>
+          <p className='caption'>...but we weren't sure they were trolling us until one of us said "Ok, and now a smiling one" and they jumped up to photobomb us again!</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='jet-photo' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Jets_game.jpg'
+          alt='Watching the Jets game'
           />
-          <p className='caption'>????</p>
+          <p className='caption'>Bill's first Jet game! It was a tough battle, but (as predicted) Claire's Jets *did* manage to lose to the Bills! (Both teams tried so hard to lose that day.) And he's marrying Claire anyway, amazing.</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='cheung-chau-photo-tall' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Cheung_Chau_sea.jpg'
+          alt='On a beach on the island of Cheung Chau, in Hong Kong (on the South China Sea)'
           />
-          <p className='caption'>????</p>
+          <p className='caption'>But it's not all sports! Here we are on the beautiful Hong Kong island of Cheung Chau, dipping our feet in the South China Sea.</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='cheung-chau-photo-wide' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Cheung_Chau_sea_wide.jpg'
+          alt='On a beach on the island of Cheung Chau, in Hong Kong (on the South China Sea)'
           />
-          <p className='caption'>????</p>
+          <p className='caption'>Bill always complains that it looks ridiculous when Claire does this to his hair, but that's no motivation to stop, so he's gonna hafta either get used to it or get more haircuts...</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='cheung-chau-photo-beach' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Cheung_Chau_beach.jpg'
+          alt='On a beach on the island of Cheung Chau, in Hong Kong (on the South China Sea)'
           />
-          <p className='caption'>????</p>
+          <p className='caption'>It was really a picturesque little island! Highly recommended for your next visit to Hong Kong.</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='gill-wedding-photo' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Gill_wedding.jpg'
+          alt="At Claire's sister Theresa's wedding"
           />
-          <p className='caption'>????</p>
+          <p className='caption'>One of our favorite photos together, taken by a cousin (thanks, Maggie!) at Claire's sister's wedding&mdash;after Bill had just spent a bunch of time meeting Claire's <em>entire</em> family for the first time, while she was busy with maid of honor duties and couldn't even run interference...</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='gill-wedding-dancing-photo' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Gill_wedding_dancing.jpg'
+          alt="At Claire's sister Theresa's wedding"
           />
-          <p className='caption'>????</p>
+          <p className='caption'>The <a href='https://maggiemillsphotography.com/' target='blank'>official photographer</a> captured this delightful photo of us on the dance floor!</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='cheung-photo' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/Cheung_wedding.jpg'
+          alt="At bridesmaid Anna's wedding in Hong Kong"
           />
-          <p className='caption'>????</p>
+          <p className='caption'>At a friend's wedding in Hong Kong, we couldn't resist posing in front of the "Love Station"!</p>
       </div>
-      <div id='????-photo' className='polaroid'>
+      <div id='ravi-photo' className='polaroid'>
         <img
           className='polaroid-image'
-          src='./images/????'
-          alt='????'
+          src='./images/ravis_wedding.jpg'
+          alt="At Ravi's wedding here in Pittsburgh"
           />
-          <p className='caption'>????</p>
+          <p className='caption'>This delightful Indian wedding reception was right here in Pittsburgh! Such a joy (and not <em>just</em> because no travel was required, though that didn't hurt).</p>
       </div>
       <div id='????-photo' className='polaroid'>
         <img
