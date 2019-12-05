ebb3d9f3f6040160fdefc4923a97fc7c7099eabb
Claire Gilligan <feminaprovita@gmail.com>
2019-12-05 01:35:34 -0500

    more photos, some copy

--- client/components/Photos.js
+++ client/components/Photos.js
@@ -3,6 +3,201 @@ import React from 'react'
 const Photos = () => (
   <div id='photo-container' className='page-container'>
     <h1>Photos of Us</h1>
+    <div id='some-photos'>
+      <div id='subway-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/subway.jpg'
+          alt='On the subway'
+          />
+          <p className='caption'>Our very first photo (on the NYC subway, specifically the N train)</p>
+      </div>
+      <div id='graffiti-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/graffiti.jpg'
+          alt='In front of some sappy graffiti'
+          />
+          <p className='caption'>The graffiti behind us reads, "Happiness is in the air" (And yes, Claire was walking around with a blanket while Bill was comfortably wearing no coat; don't judge)</p>
+      </div>
+      <div id='cloisters-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/Met_Cloisters.jpg'
+          alt='Outside the Cloisters'
+          />
+          <p className='caption'>This was taken outside the Met Cloisters, the first museum we visited together</p>
+      </div>
+      <div id='cloisters-kiss-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/Met_Cloisters_kiss.jpg'
+          alt='Outside the Cloisters'
+          />
+          <p className='caption'>More firsts! A surprise kiss photo.</p>
+      </div>
+      <div id='pirates-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/Pirates_game.jpg'
+          alt='pirates game'
+          />
+          <p className='caption'>Claire's first Pirates game</p>
+      </div>
+      <div id='penguins-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/Pens_game.jpg'
+          alt='penguins game'
+          />
+          <p className='caption'>Claire's first Penguins game</p>
+      </div>
+      <div id='steeler-photo-sad' className='polaroid-sideways'>
+        <img
+          className='polaroid-image'
+          src='./images/steeler_game_first_sad.jpg'
+          alt='Silly Steeler gameday photo'
+          />
+          <p className='caption'>As you've clearly noticed by now, we've been trying to take a picture at every notable sports game. These guys behind us were making faces as we framed our sad photo (because the Steelers were playing poorly)...</p>
+      </div>
+      <div id='steeler-photo-happy' className='polaroid-sideways'>
+        <img
+          className='polaroid-image rotate-back'
+          src='./images/steeler_game_first_happy.jpg'
+          alt='Silly Steeler gameday photo'
+          />
+          <p className='caption rotate-back'>...but we weren't sure they were trolling us until one of us said "Ok, and now a smiling one" and they jumped forward to photobomb us again!</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+      <div id='apron-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/aprons.jpg'
+          alt='Wearing silly aprons'
+          />
+          <p className='caption'>Adjusting to domestic life by cooking, and silly about it!</p>
+      </div>
+      <div id='????-photo' className='polaroid'>
+        <img
+          className='polaroid-image'
+          src='./images/????'
+          alt='????'
+          />
+          <p className='caption'>????</p>
+      </div>
+
+    </div>
     <img
       className='under-construction small-image'
       src='./images/under_construction_geek_woman.png'
