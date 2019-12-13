16f8332dcd711ebc1cd053c873f31d55d2b1d758
Claire Gilligan <feminaprovita@gmail.com>
2019-12-13 15:33:09 -0500

    new photo branch set up

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => (
     <div id='navlink-container'>
       <Link to='/' id='navlink-home' className='navlink-one'>Home</Link>
       <Link to='/ourstory' id='navlink-ourstory' className='navlink-one'>Our Story</Link>
-      {/* <Link to='/photos' id='navlink-photos' className='navlink-one'>Photos</Link> */}
+      <Link to='/photos' id='navlink-photos' className='navlink-one'>Photos</Link>
       <Link to='/schedule' id='navlink-schedule' className='navlink-one'>Schedule</Link>
       <Link to='/attractions' id='navlink-attractions' className='navlink-one'>Attractions</Link>
       <Link to='/registry' id='navlink-registry' className='navlink-one'>Registry</Link>
