240527f7d46b958a9f41541d5dee82b8e4d7784e
Claire Gilligan <feminaprovita@gmail.com>
2020-02-13 23:48:25 -0500

    styled our new story

--- client/components/OurStory.js
+++ client/components/OurStory.js
@@ -4,59 +4,79 @@ import {Link} from 'react-router-dom'
 const OurStory = () => (
   <div id='ourstory-container' className='page-container'>
     <h1>Our Story</h1>
-    <h2 className='setup'>THE SETUP</h2>
-    <p className='setup'>So the key here is what we'll call Bill's college debate
-    club (its actual designation is more complicated, but isn't everything?). Its
-    members have a very strong sense of community, and it's normal to be active
-    at alumni events long past when the people you knew graduated (and stay up
-    half the night talking philosophy with friends and strangers alike). Now, this
-    group attracts strong personalities, and so members of this group tend to tell
-    stories about their brethren.</p>
-    <p className='setup'>A brain like Claire's tries to catalog such people, to
-    connect one story to another, and one day a friend was telling her about a
-    conversation with a heretofore unmentioned friend from said debate club. "If
-    I were ever to set you up with anybody," he explained, "this would be the guy!" </p>
-    <p className='setup'>He thought this was a safe thing to say, as she'd had a
-    longstanding aversion to being set up! Unbeknownst to him, she'd quietly
-    changed her tune; so she asked him to please follow this impulse! Buuuuuuut
-    he communicates like your stereotypical straight man, so he waited until the
-    next time he saw Bill&mdash;seven months later! (In the meantime, two other
-    friends from the same group were brought in on the idea, and it became a Plan.)</p>
-    <p className='setup'>On that day, seven months later, Bill was attending the
-    wedding of another friend from this debate club. He was barely three steps
-    inside the gate when one of the Schemers made a beeline for him, greeting him
-    with, "So Bill, are you still looking for a wife?"</p>
-    <p className='setup'>Two hours later, the first friend cautiously broached
-    the subject in conversation, careful not to push Bill into anything he wasn't
-    up for (thoughtful, appreciated)&mdash;and was furious to learn he'd been
-    beaten to the punch!</p>
+    <div className='story-holder' id='setup-container'>
+      <img
+        className='small-image story-img'
+        id='story-subway'
+        src='./images/Subway.jpg'
+        alt='On the subway (our first photo together)'
+        />
+      <div id='setup-story'>
+        <h2>THE SETUP</h2>
+        <p>So the key here is what we'll call Bill's college debate
+        club (its actual designation is more complicated, but isn't everything?). Its
+        members have a very strong sense of community, and it's normal to be active
+        at alumni events long past when the people you knew graduated (and stay up
+        half the night talking philosophy with friends and strangers alike). Now, this
+        group attracts strong personalities, and so members of this group tend to tell
+        stories about their brethren.</p>
+        <p>A brain like Claire's tries to catalog such people, to
+        connect one story to another, and one day a friend was telling her about a
+        conversation with a heretofore unmentioned friend from said debate club. "If
+        I were ever to set you up with anybody," he explained, "this would be the guy!" </p>
+        <p>He thought this was a safe thing to say, as she'd had a
+        longstanding aversion to being set up! Unbeknownst to him, she'd quietly
+        changed her tune; so she asked him to please follow this impulse! Buuuuuuut
+        he communicates like your stereotypical straight man, so he waited until the
+        next time he saw Bill&mdash;seven months later! (In the meantime, two other
+        friends from the same group were brought in on the idea, and it became a Plan.)</p>
+        <p>On that day, seven months later, Bill was attending the
+        wedding of another friend from this debate club. He was barely three steps
+        inside the gate when one of the Schemers made a beeline for him, greeting him
+        with, "So Bill, are you still looking for a wife?"</p>
+        <p>Two hours later, the first friend cautiously broached
+        the subject in conversation, careful not to push Bill into anything he wasn't
+        up for (thoughtful, appreciated)&mdash;and was furious to learn he'd been
+        beaten to the punch!</p>
+      </div>
+    </div>
 
-    <h2 className='proposal'>THE PROPOSAL</h2>
-    <p className='proposal'>Ring arrived Monday. Tuesday they left to visit Claire's
-    family for Thanksgiving. Bill had a small window. He'd planned to bring Claire
-    to Mass and adoration Tuesday morning and propose just outside the church, a
-    lovely moment reflective of their relationship.</p>
-    <p className='proposal'>But Claire had been stuck working very late the previous
-    night (like 3am late). Claire's sister encouraged Bill's idea of a backup plan.
-    When he tried to wake Claire before 7, she held a whole conversation with him
-    about how badly she'd slept, how she really couldn't get up… (Fun fact: Claire
-    was asleep through this whole conversation and remembers not a wit!) So he
-    quickly enacted plan B, and waited.</p>
-    <p className='proposal'>Claire awoke to find a note next to her pillow, with
-    some sort of cute poem thing written on the front, and a rose petal on top.
-    Not sufficiently awake to decode the poem, she opened the envelope and read a
-    wonderful, heartfelt love letter. She went to the kitchen and thanked Bill
-    for the letter… whereupon she learned that she'd been supposed to open it later. Oops.</p>
-    <p className='proposal'>So, there was a scavenger hunt! A dozen roses hidden
-    throughout the house. (The poem implied this, which she'd missed in her groggy
-    state.) With verbal directions, she wandered around and joyfully found them all,
-    finishing by placing them in her vase, which had some pretty red stuff in the
-    bottom, preventing the roses from standing. She pulled the red stuff out...and
-    a ring box tumbled out! He got to his knees, and the rest, as they say, is history.</p>
+    <div className='story-holder' id='proposal-container'>
+      <div id='proposal-story'>
+        <h2 className='proposal'>THE PROPOSAL</h2>
+        <p>Ring arrived Monday. Tuesday they left to visit Claire's
+        family for Thanksgiving. Bill had a small window. He'd planned to bring Claire
+        to Mass and adoration Tuesday morning and propose just outside the church, a
+        lovely moment reflective of their relationship.</p>
+        <p>But Claire had been stuck working very late the previous
+        night (like 3am late). Claire's sister encouraged Bill's idea of a backup plan.
+        When he tried to wake Claire before 7, she held a whole conversation with him
+        about how badly she'd slept, how she really couldn't get up… (Fun fact: Claire
+        was asleep through this whole conversation and remembers not a wit!) So he
+        quickly enacted plan B, and waited.</p>
+        <p>Claire awoke to find a note next to her pillow, with
+        some sort of cute poem thing written on the front, and a rose petal on top.
+        Not sufficiently awake to decode the poem, she opened the envelope and read a
+        wonderful, heartfelt love letter. She went to the kitchen and thanked Bill
+        for the letter… whereupon she learned that she'd been supposed to open it later. Oops.</p>
+        <p>So, there was a scavenger hunt! A dozen roses hidden
+        throughout the house. (The poem implied this, which she'd missed in her groggy
+        state.) With verbal directions, she wandered around and joyfully found them all,
+        finishing by placing them in her vase, which had some pretty red stuff in the
+        bottom, preventing the roses from standing. She pulled the red stuff out...and
+        a ring box tumbled out! He got to his knees, and the rest, as they say, is history.</p>
+      </div>
+      <img
+        className='small-image story-img'
+        id='story-ring'
+        src='./images/ring_with_flowers.jpg'
+        alt='Ring with roses'
+        />
+    </div>
 
-    <p className='comment'><em>For any of you who are gluttons for punishment,
-      our overly detailed first draft is hidden <Link to='/ourstorylong'>here</Link>,
-      for your reading pleasure...</em></p>
+    <p className='comment'><em>For any of you gluttons for punishment, our overly
+      detailed first draft is hidden <Link to='/ourstorylong'>here</Link>, for your
+      reading pleasure...</em></p>
   </div>
 )
 
