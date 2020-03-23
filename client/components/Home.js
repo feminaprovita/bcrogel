a3896a3a6ce0ae618f2086d9756600e206497484
Claire Gilligan <feminaprovita@gmail.com>
2020-03-23 13:40:37 -0400

    updated with COVID-19 uncertainties

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,13 +4,19 @@ import countdown from '../countdown'
 const Home = () => (
   <div id='homepage-container' className='page-container'>
     <p id='countdown-text'>{countdown} #SwellToBeRogel</p>
+    <p>Hopefully&mdash;we're keeping an eye on COVID&ndash;19, neither canceling
+      anything prematurely nor exposing you foolhardily to each other's germs before
+      medical authorities deem that safe. The best way to keep on top of our changes
+      is to <a href='http://swelltoberogel.rsvpify.com' target='blank'>RSVP</a> with
+      email address.</p>
     <img
       id='homepage-pic'
-      src='images/Gill_wedding_spiffed.jpg'
-      alt="Bill and Claire at Claire's sister Theresa's wedding in Charlotte"
+      src='images/engagement_25.jpg'
+      alt="One of our favorite engagement photos"
     />
     <p>We're so excited to celebrate with you!</p>
-    <p>All the stuff you'll need will be here. Sooner or later.</p>
+    <p>All the stuff you'll need should be here. If not, <a href='mailto:bcrogel@gmail.com'>drop
+    us a line</a>!</p>
   </div>
 )
 
