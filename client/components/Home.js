7424ed8adc5f42f3827e832901c3e6d7f56d3d9a
Claire Gilligan <feminaprovita@gmail.com>
2020-04-05 21:20:48 -0400

    added disclaimer to home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,18 +4,20 @@ import countdown from '../countdown'
 const Home = () => (
   <div id='homepage-container' className='page-container'>
     <p id='countdown-text'>{countdown} #SwellToBeRogel</p>
+    <p className='coronavirus-home'>Well, God willing and pandemic permitting.</p>
+    <p className='coronavirus-home'>We probably won't know till sometime in May
+    whether and how we'll need to alter our plans...</p>
     <img
       id='homepage-pic'
       src='images/engagement_25.jpg'
       alt="One of our favorite engagement photos"
     />
-    <p>Well, hopefully&mdash;we're keeping an eye on COVID&ndash;19, neither canceling
-      anything prematurely nor exposing you foolhardily to each other's germs before
-      medical authorities deem that safe. The best way to keep on top of our changes
-      is to <a href='http://swelltoberogel.rsvpify.com' target='blank'>RSVP</a> with
-      an email address.</p>
-    <p>We're so excited to celebrate with you! All the stuff you'll need should
-      be here. If not, <a href='mailto:bcrogel@gmail.com'>drop us a line</a>!</p>
+    <p className='coronavirus-home'>So please reserve and <a
+    href='http://swelltoberogel.rsvpify.com' target='blank'>RSVP</a> with hope!
+    And <a href='mailto:bcrogel@gmail.com'>drop us a line</a> with anything else
+    you may need.</p>
+    <p>We're so excited to celebrate with you! Whenever we can. You're in our
+      prayers! 🙏🏻🙏🏻</p>
   </div>
 )
 
