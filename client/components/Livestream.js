a46e8e8efd9787eb99e2fcbad35dbeeec0bc34c3
Claire Gilligan <feminaprovita@gmail.com>
2020-06-17 08:50:50 -0400

    added baseline Mass program text

--- client/components/Livestream.js
+++ client/components/Livestream.js
@@ -3,7 +3,244 @@ import countdown from '../countdown'
 
 const Livestream = () => (
   <div id='livestream-container' className='page-container'>
-    <p>There will be an email signup here. It's just not ready set.</p>
+    {/* <p>There will be an email signup here. It's just not ready set.</p> */}
+    <div id='program-text'>
+        <h3>Preludes</h3>
+          <h4>If Ye Love Me –Thomas Tallis</h4>
+            <p>If ye love me, keep my commandments. <br/>
+              And I will pray the Father, <br/>
+              and he shall give you another comforter, <br/>
+              that he may 'bide with you forever; <br/>
+              E'en the sp'rit of truth. <br/>
+              —Jn 14:15-17</p>
+          <h4>God So Loved the World –John Stainer</h4>
+            <p>God so loved the world<br/>
+            that he gave his only begotten Son,<br/>
+            that whoso believeth in him<br/>
+            should not perish, but have everlasting life.<br/>
+            For God sent not his Son into the world to condemn the world,<br/>
+            but that the world through him might be saved.</p>
+        <h3>Processions</h3>
+          <h4>Entrance Antiphon (Tb 7:15; 8:19; Ps 127:1)</h4>
+            <p><em>Deus Israel conjungat vos, <br/>
+            et ipse sit vobiscum, <br/>
+            qui misertus est duobus unicis: <br/>
+            et nunc, Domine, fac eos plenius benedicere te. <br/>
+            Beati omnes qui timent Dominum: <br/>
+            qui ambulant in viis ejus.</em></p>
+            <p>(Tb 7:15; 8:19) May the God of Israel join you together; <br/>
+            and may he be with you, <br/>
+            who took pity upon two only children: <br/>
+            and now, O Lord, make them bless thee more fully. <br/>
+            (Ps 127:1) Blessed are all they that fear the Lord; <br/>
+            that walk in his ways.</p>
+          <h4>Ubi Caritas –Maurice Duruflé</h4>
+            <p><em>Ubi caritas et amor, Deus ibi est. <br/>
+            Congregavit nos in unum Christi amor. <br/>
+            Exultemus, et in ipso iucundemur. <br/>
+            Timeamus, et amemus Deum vivum. <br/>
+            Et ex corde diligamus nos sincero. Amen.</em><p/>
+            <p>Where charity and love are, God is there. <br/>
+            Christ's love has gathered us into one. <br/>
+            Let us rejoice and be pleased in Him. <br/>
+            Let us fear, and let us love the living God. <br/>
+            And may we love each other with a sincere heart. Amen.</p>
+          <h4>Rise Up, My Love, My Fair One –Healey Willan</h4>
+            <p>Rise up my love, my fair one, and come away.
+            For lo, the winter is past, the rain is over and gone.<br/>
+            The flowers appear upon the earth.<br/>
+            The time of singing of birds is come.<br/>
+            Arise my love, my fair one, and come away.</p>
+        <h3>Introductory Rites</h3>
+          <h4>Kyrie</h4>
+            <p>Lord, have mercy. Lord, have mercy.<br/>
+            Christ, have mercy. Christ, have mercy.<br/>
+            Lord, have mercy. Lord, have mercy.</p>
+          <h4>Gloria</h4>
+            <p>Glory to God in the highest,<br/>
+            and on earth peace to people of good will.<br/>
+            We praise you,<br/>
+            we bless you,<br/>
+            we adore you,<br/>
+            we glorify you,<br/>
+            we give you thanks for your great glory,<br/>
+            Lord God, heavenly King,<br/>
+            O God, almighty Father.<br/>
+            Lord Jesus Christ, Only Begotten Son,<br/>
+            Lord God, Lamb of God, Son of the Father,<br/>
+            you take away the sins of the world,<br/>
+            have mercy on us;<br/>
+            you take away the sins of the world,<br/>
+            receive our prayer;<br/>
+            you are seated at the right hand of the Father,<br/>
+            have mercy on us.<br/>
+            For you alone are the Holy One,<br/>
+            you alone are the Lord,<br/>
+            you alone are the Most High,<br/>
+            Jesus Christ,<br/>
+            with the Holy Spirit,<br/>
+            in the glory of God the Father.<br/>
+            Amen.</p>
+          <h4>Collect</h4>
+      <h2>The Liturgy of the Word</h2>
+          <h4>First Reading (Tb 8:4b-8)</h4>
+            <p><em>Tobiah and Sarah's wedding night prayer</em></p>
+          <h4>Responsorial Psalm (Ps 34)</h4>
+            <p>I will bless the Lord at all times</p>
+          <h4>Second Reading (Rv 19:1, 5-9a)</h4>
+            <p><em>Blessed are those who have been called to the wedding feast of the Lamb</em></p>
+          <h4>Alleluia</h4>
+          <h4>Gospel (Jn 2:2-11)</h4>
+            <p><em>Jesus did this as the beginning of his signs in Cana in Galilee</em></p>
+          <h4>Homily</h4>
+      <h2>The Celebration of Matrimony</h2>
+          <h4>Questions before the Consent</h4>
+          <h4>The Consent (Vows)</h4>
+          <h4>Reception of the Consent</h4>
+          <h4>Blessing and Giving of Rings</h4>
+          <h4>Universal Prayer (Prayer of the Faithful)</h4>
+      <h2>The Liturgy of the Eucharist</h2>
+        <h3>Offertory</h3>
+          <p><em>*ad orientem note*</em></p>
+          <h4>Offertory Antiphon (Ps 31:15-16)</h4>
+            <p><em>In te speravi, Domine: dixi: Tu es Deus meus, in manibus tuis tempora mea.</em></p>
+            <p>In you I have put my trust, O Lord; I said: "You are my God, my destiny is in your hands."</p>
+          <h4>Rejoice in the Lord Alway –unknown (Phil 4:4-7)</h4>
+            <p>Rejoice in the Lord alway, and again I say rejoice.<br/>
+            Let your softness be known unto all men.<br/>
+            The Lord is e'en at hand.<br/>
+            Be careful for nothing,<br/>
+            but in all prayer and supplication<br/>
+            Let your petitions be manifest unto God with giving of thanks.<br/>
+            Then the peace of God, which passes all understanding, <br/>
+            keep your hearts and minds through Christ Jesus. Amen.</p>
+          <h4>Prayer over the Offerings</h4>
+            <h5 className='response'>Response:</h5>
+              <p>Priest: Pray, brothers and sisters,<br/>
+              that my sacrifice and yours<br/>
+              may be acceptable to God,<br/>
+              the almighty Father.</p>
+              <p>All: May the Lord accept the sacrifice at your hands<br/>
+              for the praise and glory of his name,<br/>
+              for our good<br/>
+              and the good of all his holy Church.</p>
+          <h4>Preface</h4>
+          <h4>Sanctus</h4>
+            <p>Holy, Holy, Holy Lord God of hosts.<br/>
+            Heaven and earth are full of your glory.<br/>
+            Hosanna in the highest.<br/>
+            Blessed is he who comes in the name of the Lord.<br/>
+            Hosanna in the highest.</p>
+          <h4>Roman Canon (Eucharistic Prayer I)</h4>
+          <h4>Memorial Acclamation</h4>
+            <p>Save us, Savior of the world,<br/>
+            for by your Cross and Resurrection<br/>
+            you have set us free.</p>
+          <h4>Our Father</h4>
+          <h4>Nuptial Blessing</h4>
+          <h4>Sign of Peace</h4>
+          <h4>Agnus Dei</h4>
+            <p>Lamb of God, you take away the sins of the world, have mercy on us.<br/>
+            Lamb of God, you take away the sins of the world, have mercy on us.<br/>
+            Lamb of God, you take away the sins of the world, grant us peace.</p>
+            <h5 className='response'>Response:</h5>
+              <p>Priest: Behold the Lamb of God,<br/>
+              behold him who takes away the sins of the world.<br/>
+              Blessed are those called to the supper of the Lamb.</p>
+              <p>All: Lord, I am not worthy<br/>
+              that you should enter under my roof,<br/>
+              but only say the word<br/>
+              and my soul shall be healed.</p>
+        <h3>Communion Rite</h3>
+          <h4>Prayer of Spiritual Communion</h4>
+            <p>At your feet, O my Jesus,<br/>
+            I prostrate myself and I offer you<br/>
+            repentance of my contrite heart,<br/>
+            which is humbled in its nothingness<br/>
+            and in your holy presence.<br/>
+            I adore you in the Sacrament of your love,<br/>
+            the ineffable Eucharist.<br/>
+            I desire to receive you <br/>
+            into the poor dwelling that my heart offers you.<br/>
+            While waiting for the happiness of sacramental communion,<br/>
+            I wish to possess you in spirit.<br/>
+            Come to me, O my Jesus,<br/>
+            since I, for my part, am coming to you!<br/>
+            May your love embrace my whole being in life and in death.<br/>
+            I believe in you,<br/>
+            I hope in you,<br/>
+            I love you. Amen.<br/>
+            (Cardinal Rafael Merry del Val, via <a href='https://www.vaticannews.va/en/prayers/the-spiritual-communion.html' target='blank'>vaticannews.va</a>)</p>
+          <h4>Communion Antiphon –Adam Bartlett (Mt 6:33)</h4>
+            <p>Seek first the kingdom of God, and all the rest will be given to you in addition, says the Lord.</p>
+          <h4>Ave Verum Corpus –William Byrd</h4>
+            <p><em>Ave verum Corpus natum<br/>
+            De Maria Virgine:<br/>
+            Vere passum, immolatum<br/>
+            In cruce pro homine:<br/>
+            Cuius latus perforatum<br/>
+            Fluxit aqua et sanguine:<br/>
+            Esto nobis praegustatum<br/>
+            Mortis in examine.<br/>
+            O Iesu dulcis!<br/>
+            O Iesu pie!<br/>
+            O Iesu Fili Mariae.</em></p>
+            <p>Hail, true Body, <br/>
+            Born of the Virgin Mary,<br/>
+            Who truly suffered, sacrificed<br/>
+            On the Cross for all mankind,<br/>
+            From whose pierced side <br/>
+            Flowed water and blood.<br/>
+            Be for us a foretaste<br/>
+            In the test of death.<br/>
+            O sweet Jesus,<br/>
+            O merciful Jesus,<br/>
+            Son of Mary.</p>
+        <h3>Marian Procession</h3>
+          <h4>Ave Maria –Tomás Luis de Victoria</h4>
+            <p><em>Ave Maria, gratia plena, Dominus tecum;<br/>
+            benedicta tu in mulieribus,<br/>
+            et benedictus fructus ventris tui, Jesus Christus.<br/>
+            Sancta Maria, Mater Dei,<br/>
+            ora pro nobis peccatoribus,<br/>
+            nunc et in hora mortis nostrae. Amen.</em></p>
+            <p>Hail Mary, full of grace, the Lord is with thee;<br/>
+            blessed art thou among women,<br/>
+            and blessed is the fruit of thy womb, Jesus [Christ].<br/>
+            Holy Mary, Mother of God,<br/>
+            pray for us sinners,<br/>
+            now and at the hour of our death. Amen.</p>
+          <h4>Prayer after Communion</h4>
+        <h3>Concluding Rites</h3>
+          <h4>Dismissal</h4>
+          <h4>Solemn Blessing</h4>
+          <h4>Announcement of the Couple</h4>
+          <h4>Holy God, We Praise Thy Name –attr. Ignace Franz, tr. Clarence A. Walworth</h4>
+            <p>Holy God, we praise thy name;<br/>
+            Lord of all, we bow before thee.<br/>
+            All on earth thy scepter claim,<br/>
+            All in heav'n above adore thee.<br/>
+            Infinite thy vast domain,<br/>
+            Everlasting is thy reign.<br/>
+            Infinite thy vast domain,<br/>
+            Everlasting is thy reign.<br/><br/>
+            Hark! the glad celestial hymn<br/>
+            Angel choirs above are raising;<br/>
+            Cherubim and seraphim,<br/>
+            In unceasing chorus praising,<br/>
+            Fill the heav'ns with sweet accord:<br/>
+            Holy, holy, holy Lord!<br/>
+            Fill the heav'ns with sweet accord:<br/>
+            Holy, holy, holy Lord!<br/><br/>
+            Holy Father, holy Son,<br/>
+            Holy Spirit, three we name thee;<br/>
+            Though in essence only one,<br/>
+            Undivided God we claim thee<br/>
+            And, adoring, bend the knee<br/>
+            While we own the mystery.<br/>
+            And, adoring, bend the knee<br/>
+            While we own the mystery.</p></p>
+    </div>
   </div>
 )
 
